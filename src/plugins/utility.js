789910c1d8a4b1c0ac2ccf361052d53b3593a146
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-01-13 17:31:08 -0500

    Use arrow funciton that immediately return obj literal syntax.

--- src/plugins/utility.js
+++ src/plugins/utility.js
@@ -1,20 +1,18 @@
-const conditional = function(expression, if_block, else_block, when) {
-    return {
-        name: '',
-        plugin: {
-            args: {
-                action: if_block.plugin,
-                else_action: else_block.plugin,
-                expression
-            },
-            label: "Utility - Conditional",
-            method: "conditional",
-            module: "utility",
-            name: "utility"
+const conditional = (expression, if_block, else_block, when) => ({
+    name: '',
+    plugin: {
+        args: {
+            action: if_block.plugin,
+            else_action: else_block.plugin,
+            expression
         },
-        when
-    }
-};
+        label: "Utility - Conditional",
+        method: "conditional",
+        module: "utility",
+        name: "utility"
+    },
+    when
+});
 
 export const utility = {
     conditional
