c404cde1014fdb2f9c8577a7332fdb064c12a6ff
Walker Randolph Smith <walkerrandolphsmith@gmail.com>
2017-01-13 22:24:02 -0500

    Use single quotes

--- src/plugins/bitBucketCloud/index.spec.js
+++ src/plugins/bitBucketCloud/index.spec.js
@@ -2,17 +2,17 @@ import test from 'ava';
 import { bitBucketCloud } from './';
 
 test('bitBucketCloud plugin has api_call', t => {
-    t.true(bitBucketCloud.hasOwnProperty("api_call"))
+    t.true(bitBucketCloud.hasOwnProperty('api_call'))
 });
 
 test('bitBucketCloud plugin has create_branch', t => {
-    t.true(bitBucketCloud.hasOwnProperty("create_branch"))
+    t.true(bitBucketCloud.hasOwnProperty('create_branch'))
 });
 
 test('bitBucketCloud plugin has delete_branch', t => {
-    t.true(bitBucketCloud.hasOwnProperty("delete_branch"))
+    t.true(bitBucketCloud.hasOwnProperty('delete_branch'))
 });
 
 test('bitBucketCloud plugin has show_summary', t => {
-    t.true(bitBucketCloud.hasOwnProperty("show_summary"))
+    t.true(bitBucketCloud.hasOwnProperty('show_summary'))
 });
