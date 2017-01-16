c2f2e6e90fbbf7e09bbb331205a733a75b39f1f0
Alejandro Guzman <alejandro.guzman@versionone.com>
2017-01-16 11:53:54 -0500

    wait for data

--- src/plugins/utility/index.js
+++ src/plugins/utility/index.js
@@ -2,10 +2,12 @@ import { conditional } from './conditional';
 import { email_contributors } from './email_contributors';
 import { get_registry } from './get_registry';
 import { set_registry } from './set_registry';
+import { wait_for_data } from './wait_for_data';
 
 export const utility = {
     conditional,
     email_contributors,
     get_registry,
-    set_registry
+    set_registry,
+    wait_for_data
 };
---
+++ src/plugins/utility/wait_for_data.js
@@ -0,0 +1,17 @@
+import { ALWAYS } from './../constants';
+import { get_definition } from './../base';
+
+export const wait_for_data = (expression, status, wait_seconds, when=ALWAYS,
+    additional_args={}, description='') => get_definition({
+        expression,
+        status,
+        wait_seconds
+    },
+    {
+        label: 'Utility - Wait For Data',
+        method: 'wait_for_data',
+        module: 'utility',
+        name: 'utility'
+    },
+    when, additional_args, description
+);
