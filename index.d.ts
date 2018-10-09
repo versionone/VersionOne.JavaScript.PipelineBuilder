72d24310e5ee67034ea19b8ceae4fc5b2f57b9ee
Alejandro Guzman <a.guillermo.guzman@gmail.com>
2018-10-09 16:23:20 -0400

    update types on package json

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,3 @@
 export function create_pipeline(name: string, description: string, phases: object[]): object;
 export function create_phase(name: string, stages: object[]): object;
 export function create_stage(name: string, steps: object[]): object;
-export interface plugins {
-    jenkins: {
-        build (
-            job_name,
-            parameters={},
-            wait,
-            inherit_job_status,
-            regular_expression,
-            artifact_name,
-            instance_name,
-            when, 
-            additional_args,
-            description
-            ): object
-    }
-    [key: string]: () => {}
-}
