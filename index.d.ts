736f9354866fe82c9cfa71d992bb2c4840783837
Keith Stewart <kgstewart@gmail.com>
2018-10-10 09:45:15 -0500

    add constants to TS definition

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,18 @@
 export function create_pipeline(name: string, description: string, phases: object[]): object;
 export function create_phase(name: string, stages: object[]): object;
 export function create_stage(name: string, steps: object[]): object;
+
+interface CONSTANTS {
+  ALWAYS: 'always',
+  CONDITIONALLY: 'conditionally',
+  NEVER: 'never',
+  GET: 'get',
+  POST: 'post',
+  PUT: 'put',
+  DELETE: 'delete',
+  PATCH: 'patch',
+  SUCCESS: 'end-success',
+  FAILURE: 'end-failure',
+  CANCELED: 'end-canceled',
+}
+export const constants: CONSTANTS;
